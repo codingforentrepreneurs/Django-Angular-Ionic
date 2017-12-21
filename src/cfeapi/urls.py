1fbaf30787a11ee919dae5ef1213a8a207e004d2
CFE <hello@teamcfe.com>
2017-12-21 14:20:30 -0800

    4 - Add Bootstrap for API Home

--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -4,8 +4,9 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'), # 2.0 re_path, path
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'), # 2.0 re_path, path
     url(r'^admin/', admin.site.urls),
+    url(r'^api/$', TemplateView.as_view(template_name='api-home.html'), name='home'),
     url(r'^api/auth/', include('accounts.api.urls', namespace='api-auth')),
     url(r'^api/user/', include('accounts.api.user.urls', namespace='api-user')),
     url(r'^api/status/', include('status.api.urls', namespace='api-status')),
