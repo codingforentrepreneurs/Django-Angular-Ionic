47ccff21139ef78e2f371a92de3df9366e064403
CFE <hello@teamcfe.com>
2017-12-21 16:28:58 -0800

    7 - Angular Routing in Django URLs

---
+++ client/dj-ion/build-notes.md
@@ -0,0 +1,5 @@
+## Build to Django
+
+```
+ng build --prod --output-hash none --output-path /Users/cfe/dev/djionic/src/static/app/ 
+```
--- src/cfeapi/urls.py
+++ src/cfeapi/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='ng.html'), name='home'), # 2.0 re_path, path
     url(r'^admin/', admin.site.urls),
     # url(r'^api/$', TemplateView.as_view(template_name='api-home.html'), name='home'),
     url(r'^api/', include_docs_urls(title='Dj Ionic APIs')),
@@ -14,3 +13,12 @@ urlpatterns = [
     url(r'^api/status/', include('status.api.urls', namespace='api-status')),
     # url(r'^api/updates/', include('updates.api.urls')), 
 ]
+
+
+urlpatterns += [
+    url(r'^(?P<path>.*)', TemplateView.as_view(template_name='ng.html'), name='home'),
+]
+
+
+# Angular Handles 404
+# Angular Handle 500 errors
